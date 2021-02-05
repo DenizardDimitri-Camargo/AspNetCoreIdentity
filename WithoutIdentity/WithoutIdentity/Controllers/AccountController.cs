e999cdb9f897212b433888cc1a451eb745da79b4
DenizardDimitri-Camargo <62820018+DenizardDimitri-Camargo@users.noreply.github.com>
2021-02-05 13:17:28 -0400

    PROJETO EXISTENTE - Criando o Login do Usuário(nome de commit correto)
    no commit anterior foi feito o REGISTRO, neste foi feito o LOGIN

--- WithoutIdentity/WithoutIdentity/Controllers/AccountController.cs
+++ WithoutIdentity/WithoutIdentity/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -60,6 +61,48 @@ namespace WithoutIdentity.Controllers
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description); //retorna os erros para a view, caso tenha erro
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme); //remove os cookies externos
+
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.Rememberme, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(returnUrl))
+                    {
+                        return RedirectToLocal(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Tentativa de login inválida");
                 }
             }
 
