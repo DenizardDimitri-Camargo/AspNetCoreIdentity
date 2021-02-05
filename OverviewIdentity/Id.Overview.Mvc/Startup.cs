6209f3bdf9b1c8a6779b74b70e379ce530ba43e7
DenizardDimitri-Camargo <62820018+DenizardDimitri-Camargo@users.noreply.github.com>
2021-02-04 21:52:26 -0400

    IDENTITYOPTIONS - CookieSettings

--- OverviewIdentity/Id.Overview.Mvc/Startup.cs
+++ OverviewIdentity/Id.Overview.Mvc/Startup.cs
@@ -35,7 +35,6 @@ namespace Id.Overview.Mvc
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); //quant. de tempo que o user ficará bloqueado ao acontecer um lockout
                 options.Lockout.MaxFailedAccessAttempts = 5; //n° de tentativas antes do lockout(caso lockout = true)
 
-
                 //PASSWORD
                 options.Password.RequireDigit = true; //obriga ter um número entre 0-9 na senha
                 options.Password.RequiredLength = 6; 
@@ -61,6 +60,30 @@ namespace Id.Overview.Mvc
             }) 
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.AccessDeniedPath = "/Account/AccessDenied";//informa o path para o handler que trata o 403 (forbidden)... ele redireciona
+                //options.ClaimsIssuer = ""; 
+                //options.Cookie.Domain = ""; //define o cookie ao qual ele pertence, não será conf. pois faremos só localhost
+                //options.Cookie.Expiration = ;
+                options.Cookie.HttpOnly = true; //cookie pode ser acessado pelo client side
+                options.Cookie.Name = ".AspNetCore.Cookies";
+                options.Cookie.Path = "";
+                options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax; //velo de nuevo min 4:30
+                options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
+                //options.CookieManager = //define componente para obter os cookies do request ou response
+                //options.DataProtectionProvider
+                //options.Events;
+                //options.EventsType = 
+                options.ExpireTimeSpan = TimeSpan.FromDays(14); //tempo que o ticket de autenticação no cookie permanecerá válido
+                options.LoginPath = "/Account/Login"; //legal
+                options.LogoutPath = "/Account/Logout";
+                options.ReturnUrlParameter = "ReturnUrl"; //nome da var
+                //options.SessionStore = 
+                options.SlidingExpiration = true; //renova o ExpireTimeSpan
+                //options.TicketDataFormat = 
+            });
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
