bce43afaee32730f68d61b449f942c1cd78596b6
DenizardDimitri-Camargo <62820018+DenizardDimitri-Camargo@users.noreply.github.com>
2021-02-06 12:53:31 -0400

    PROJETO EXISTENTE - Criando o tela de alteração de senha
    Adiconei o Status message que ele esqueceu

--- WithoutIdentity/WithoutIdentity/Controllers/ManageController.cs
+++ WithoutIdentity/WithoutIdentity/Controllers/ManageController.cs
@@ -88,5 +88,54 @@ namespace WithoutIdentity.Controllers
 
             return (RedirectToAction(nameof(Index)));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ChangePassword()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                throw new ApplicationException($"Não foi possível carregar os dados do usuário com Id '{_userManager.GetUserId(User)}'");
+            }
+
+            var model = new ChangePasswordViewModel { StatusMessage = StatusMessage };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            
+            if (user == null)
+            {
+                throw new ApplicationException($"Não foi possível carregar os dados do usuário com Id '{_userManager.GetUserId(User)}'");
+            }
+
+            var changePasswordResult = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+
+            if (!changePasswordResult.Succeeded)
+            {
+                foreach (var error in changePasswordResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return View(model);
+            }
+
+            await _signInManager.SignInAsync(user, isPersistent: false); //faz um login, pois a senha mudou
+
+            StatusMessage = "Sua senha foi alterada com sucesso";
+
+            return RedirectToAction(nameof(ChangePassword));
+        }
     }
 }
